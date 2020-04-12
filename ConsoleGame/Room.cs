01410f15b1151ede3ae79b693fee4d443ccff083
Alisa Semenova <tantumalice@gmail.com>
2020-04-12 19:06:34 +0500

    Комната

--- ConsoleGame/Room.cs
+++ ConsoleGame/Room.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ConsoleGame.GameObjects;
 
 namespace ConsoleGame
 {
     class Room
     {
+        public List<GameObject> Items { get; protected set; }
 
+        private readonly int initialItemsCapacity = 4;
+
+        public string LookAround()
+        {
+            var sb = new StringBuilder();
+            foreach (var obj in Items)
+            {
+                sb.AppendLine(obj.ToString());
+            }
+            return sb.ToString();
+        }
+
+        public void RemoveItem(GameObject item)
+        {
+            Items.Remove(item);
+        }
+
+        public Room()
+        {
+            var rnd = new Random();
+            var count = rnd.Next(1, initialItemsCapacity);
+            Items = new List<GameObject>(count);
+            for (int i = 0; i < count; ++i)
+            {
+                Items.Add(RandomFiller.GetGameObject());
+            }
+        }
+
+        //TODO: Add item
     }
 }
