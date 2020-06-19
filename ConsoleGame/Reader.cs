9aa48872c2baf7e2482487e1056319bcd4cca48f
Alisa Semenova <tantumalice@gmail.com>
2020-06-19 22:33:40 +0500

    Dictionaries

--- ConsoleGame/Reader.cs
+++ ConsoleGame/Reader.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Text;
-
 namespace ConsoleGame
 {
     static class Reader
     {
+        public static readonly Dictionary<string, Func<string, string>> Interactions = new Dictionary<string, Func<string, string>>()
+        {
+            {"take", new Func<string, string>(command => ProcessTaking(command))},
+            {"throw", new Func<string, string>(command => ProcessThrowing(command))}
+        };
+
+        public static readonly Dictionary<string, Func<string>> SimpleCommands = new Dictionary<string, Func<string>>()
+        {
+            {"help", new Func<string>(() => Helper.GetHelpInfo())},
+            {"me", new Func<string>(() => Player.About())},
+            {"look around", new Func<string>(() => Player.Location.LookAround())},
+            {"show inventory", new Func<string>(() => Player.ShowInventory())},
+            {"show map", new Func<string>(() => Player.ShowMap())},
+            {"exit", new Func<string>(() => Helper.Exit())},
+            {"go to the west", new Func<string>(() => Player.Go("west"))},
+            {"go to the north", new Func<string>(() => Player.Go("north"))},
+            {"go to the south", new Func<string>(() => Player.Go("south"))},
+            {"go to the east", new Func<string>(() => Player.Go("east"))}
+        };
+
         public static readonly HashSet<string> Commands = new HashSet<string>
         {
             "go to the west",
@@ -86,6 +103,32 @@ namespace ConsoleGame
                     "show map" => Player.ShowMap(),
                     _ => Helper.WrongInput()
                 };
+            }
+        }
+
+        public static string ProcessTaking(string command)
+        {
+            var objName = command.Split(" ")[^1];
+            if (objName == "take")
+            {
+                return "What do you want to take? Try again!";
+            }
+            else
+            {
+                return Player.PutInInventory(objName);
+            }
+        }
+
+        public static string ProcessThrowing(string commamd)
+        {
+            var objName = command.Split(" ")[^1];
+            if (objName == "take")
+            {
+                return "What do you want to take? Try again!";
+            }
+            else
+            {
+                return Player.PutInInventory(objName);
             }
         }
 
